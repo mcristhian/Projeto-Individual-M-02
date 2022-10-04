4799922add3fa8b69b9ff26d7dbc9c8b52ca64a8
mcristhian <thematheuscris@gmail.com>
2022-10-04 20:45:46 -0300

    Implementação da Cifra de César

--- main.js
+++ main.js
@@ -1,26 +1,59 @@
 const select_forma = document.querySelector('#forma');
 const procedimento_c = document.querySelector('#codificar');
 const procedimento_d = document.querySelector('#decodificar');
+const envio = document.querySelector('#botao_envio');
 
 select_forma.addEventListener('change', (f) => {
-    f.preventDefault();
+    //f.preventDefault();
 
     if (select_forma.value == 'cesar'){
-        console.log('positivo');
         document.querySelector('.visibilidade_incremento').style.display = 'flex';
 
     } else {
-        console.log('falso');
         document.querySelector('.visibilidade_incremento').style.display = 'none';
     }
 })
 
 procedimento_c.addEventListener('click', (f) => {
-    //f.preventDefault();
-
     document.querySelector('#botao_envio').value = 'Codificar mensagem!';
 })
 
 procedimento_d.addEventListener('click', (f) => {
     document.querySelector('#botao_envio').value = 'Decodificar mensagem!'
+})
+
+envio.addEventListener('click', (cifra_de_cesar) => {
+    let alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    if (envio.value == 'Codificar mensagem!') {
+        let mensagem = document.querySelector('#mensagem').value;
+        let chave = parseInt(document.querySelector('#incremento').value);
+        let mensagem_criptografada = '';
+        
+        for(let i=0; i < mensagem.length; i++){
+            let posicao = alfabeto.indexOf(mensagem[i]);
+            
+            if (alfabeto.includes(mensagem[i])){
+                mensagem_criptografada += alfabeto[posicao + chave];
+            } else {
+                mensagem_criptografada += mensagem[i];
+            }
+        }
+        document.querySelector('#resultado').value = mensagem_criptografada;
+    } else if (envio.value == 'Decodificar mensagem!'){
+        let mensagem = document.querySelector('#mensagem').value;
+        let chave = parseInt(document.querySelector('#incremento').value);
+        let mensagem_descriptografada = '';
+        
+        for(let i=0; i < mensagem.length; i++){
+            let posicao = alfabeto.indexOf(mensagem[i]);
+            
+            if (alfabeto.includes(mensagem[i])){
+                mensagem_descriptografada += alfabeto[posicao - chave];
+            } else {
+                mensagem_descriptografada += mensagem[i];
+            }
+        }
+        document.querySelector('#resultado').value = mensagem_descriptografada;
+    }
 })
